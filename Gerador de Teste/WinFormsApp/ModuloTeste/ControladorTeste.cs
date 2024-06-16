71fbbdd0f867f08490379aef7cdc5ad44c195854
Fabis-li <fabiano_slima@hotmail.com>
2024-06-16 17:51:12 -0300

    criado controladore tabela

---
+++ Gerador de Teste/WinFormsApp/ModuloTeste/ControladorTeste.cs
@@ -0,0 +1,41 @@
+﻿using WinFormsApp.Compartilhado;
+
+namespace WinFormsApp.ModuloTeste
+{
+    public class ControladorTeste : ControladorBase
+    {
+        private IRepositorioTeste repositorioTeste;
+        public override string TipoCadastro { get { return "Teste"; } }
+
+        public override string ToolTipAdicionar { get { return "Cadastrar uma novo Teste"; } }
+
+        public override string ToolTipEditar { get { return "Editar um Teste existente"; } }
+
+        public override string ToolTipExcluir { get { return "Excluir um Teste existente"; } }
+
+        public ControladorTeste(IRepositorioTeste repositorioTeste)
+        {
+            this.repositorioTeste = repositorioTeste;
+        }
+
+        public override void Adicionar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Editar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Excluir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override UserControl ObterListagem()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gerador de Teste/WinFormsApp/ModuloTeste/TabelaTesteControl.cs
@@ -0,0 +1,48 @@
+﻿using WinFormsApp.Compartilhado;
+
+namespace WinFormsApp.ModuloTeste
+{
+    public partial class TabelaTesteControl : UserControl
+    {
+        public TabelaTesteControl()
+        {
+            InitializeComponent();
+
+            grid.Columns.AddRange(ObterColunas());
+
+            grid.ConfigurarGridSomenteLeitura();
+            grid.ConfigurarGridZebrado();
+        }
+
+        public void AtualizarRegistros(List<Teste> testes)
+        {
+            grid.Rows.Clear();
+
+            foreach (Teste t in testes)
+            {
+                grid.Rows.Add(
+                    t.Id.ToString(),
+                    t.Disciplina,
+                    t.Materia,
+                    t.QtdeQuestoes
+                    );
+            }
+        }
+
+        public int ObterRegistroSelecionado()
+        {
+            return grid.SelecionarId();
+        }
+
+        private DataGridViewColumn[] ObterColunas()
+        {
+            return new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id" },
+                new DataGridViewTextBoxColumn { DataPropertyName = "Disciplina", HeaderText = "Disciplina" },
+                new DataGridViewTextBoxColumn { DataPropertyName = "Materia", HeaderText = "Materia" },
+                new DataGridViewTextBoxColumn { DataPropertyName = "QtdeQuestoes", HeaderText = "Qtde de Questões"}
+            };
+        }
+    }
+}
