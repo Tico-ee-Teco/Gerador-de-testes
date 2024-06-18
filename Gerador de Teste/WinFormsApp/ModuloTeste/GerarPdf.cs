f2bc2ce28ee90b6633c30bb2bf93560222c0ec48
Fabis-li <fabiano_slima@hotmail.com>
2024-06-17 22:17:27 -0300

    implementado gerar pdf dos teste e do gabarito

--- Gerador de Teste/WinFormsApp/ModuloTeste/GerarPdf.cs
+++ Gerador de Teste/WinFormsApp/ModuloTeste/GerarPdf.cs
@@ -1,132 +1,189 @@
 ﻿using PdfSharp;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
-using WinFormsApp.ModuloQuestao;
+
 
 
 namespace WinFormsApp.ModuloTeste
 {
     public class GerarPdf
-    {       
+    {
         public void GerarPdfTeste(Teste teste, string caminho)
         {
             PdfDocument documento = new PdfDocument();
             documento.Info.Title = teste.Titulo;
 
+            XFont fonteTitulo = new XFont("Verdana", 20, XFontStyleEx.Bold);
+            XFont fonteCabecalho = new XFont("Verdana", 14, XFontStyleEx.Bold);
+            XFont fonteQuestao = new XFont("Verdana", 12, XFontStyleEx.Regular);
+            XFont fonteRodape = new XFont("Verdana", 10, XFontStyleEx.Regular);
+
+            int numeroPagina = 1;
+            double margemInferior = 40;
+
             PdfPage pagina = documento.AddPage();
             pagina.Size = PageSize.A4;
 
             XGraphics gfx = XGraphics.FromPdfPage(pagina);
-            XFont fonteTitulo = new XFont("Verdana", 20, XFontStyleEx.Bold);
-            XFont fonteCabecalho = new XFont("Verdana", 14, XFontStyleEx.Bold);
-            XFont fonteQuestao = new XFont("Verdana", 12, XFontStyleEx.Regular);
 
+            // Define a posição inicial yPoint
             double yPoint = 40;
 
+            // Desenha o título
             gfx.DrawString(teste.Titulo, fonteTitulo, XBrushes.Black, new XRect(0, yPoint, pagina.Width, pagina.Height), XStringFormats.TopCenter);
             yPoint += 40;
 
+            // Desenha a disciplina e a matéria
             gfx.DrawString($"Disciplina: {teste.Disciplina.Nome}", fonteCabecalho, XBrushes.Black, new XRect(40, yPoint, pagina.Width, pagina.Height), XStringFormats.TopLeft);
-            yPoint += 30;
-            gfx.DrawString($"Matéria: {teste.Materia.Nome}", fonteCabecalho, XBrushes.Black, new XRect(40, yPoint, pagina.Width, pagina.Height), XStringFormats.TopLeft);
-            yPoint += 30;
+            yPoint += 30;           
+            
+            if(teste.Materia != null)
+            {
+                gfx.DrawString($"Matéria: {teste.Materia.Nome}", fonteCabecalho, XBrushes.Black, new XRect(40, yPoint, pagina.Width, pagina.Height), XStringFormats.TopLeft);            
+                yPoint += 30;
+            }
+            else
+            {
+                gfx.DrawString($"Matéria: Prova de Recuperação", fonteCabecalho, XBrushes.Black, new XRect(40, yPoint, pagina.Width, pagina.Height), XStringFormats.TopLeft);
+                yPoint += 30;
+            }
+
+            int numeroQuestao = 1;
+            int questoesPorPagina = 5; // Inicia com 5 questões na primeira página
+            bool primeiraPagina = true;
 
             foreach (var questao in teste.Questoes)
             {
-                if (yPoint > pagina.Height - 100)
+                // Verifica se yPoint ultrapassou a altura disponível na página
+                if (!primeiraPagina && numeroQuestao % 5 == 1)
                 {
+                    // Desenha o número da página atual
+                    gfx.DrawString($"Página {numeroPagina}", fonteRodape, XBrushes.Black, new XRect(0, pagina.Height - margemInferior, pagina.Width - 40, fonteRodape.Height), XStringFormats.BottomRight);
+                    numeroPagina++;
+
+                    // Adiciona uma nova página e redefine yPoint
                     pagina = documento.AddPage();
                     pagina.Size = PageSize.A4;
                     gfx = XGraphics.FromPdfPage(pagina);
                     yPoint = 40;
+
+                    // Define o número de questões por página para 6
+                    questoesPorPagina = 6;
                 }
 
-                gfx.DrawString(questao.Enunciado, fonteQuestao, XBrushes.Black, new XRect(40, yPoint, pagina.Width - 80, pagina.Height), XStringFormats.TopLeft);
-                yPoint += 20;
-
-                foreach (var alternativa in questao.Alternativas)
+                if (primeiraPagina || numeroQuestao <= 5)
                 {
-                    gfx.DrawString($"{alternativa.Letra}) {alternativa.TextoAlternativa}", fonteQuestao, XBrushes.Black, new XRect(60, yPoint, pagina.Width - 80, pagina.Height), XStringFormats.TopLeft);
+                    // Desenha a questão
+                    gfx.DrawString($"{numeroQuestao} - {questao.Enunciado}", fonteQuestao, XBrushes.Black, new XRect(40, yPoint, pagina.Width - 80, pagina.Height), XStringFormats.TopLeft);
+                    yPoint += 20;
+
+                    // Desenha as alternativas
+                    foreach (var alternativa in questao.Alternativas)
+                    {
+                        gfx.DrawString($"{alternativa.Letra}) {alternativa.TextoAlternativa}", fonteQuestao, XBrushes.Black, new XRect(60, yPoint, pagina.Width - 80, pagina.Height), XStringFormats.TopLeft);
+                        yPoint += 20;
+                    }
                     yPoint += 20;
                 }
-                yPoint += 20;
+                else
+                {
+                    // Desenha a questão
+                    gfx.DrawString($"{numeroQuestao} - {questao.Enunciado}", fonteQuestao, XBrushes.Black, new XRect(40, yPoint, pagina.Width - 80, pagina.Height), XStringFormats.TopLeft);
+                    yPoint += 20;
+
+                    // Desenha as alternativas
+                    foreach (var alternativa in questao.Alternativas)
+                    {
+                        gfx.DrawString($"{alternativa.Letra}) {alternativa.TextoAlternativa}", fonteQuestao, XBrushes.Black, new XRect(60, yPoint, pagina.Width - 80, pagina.Height), XStringFormats.TopLeft);
+                        yPoint += 20;
+                    }
+                    yPoint += 20;
+                }
+
+                // Se estiver na última questão da primeira página, define questõesPorPagina para 6
+                if (primeiraPagina && numeroQuestao == 5)
+                {
+                    questoesPorPagina = 6;
+                    primeiraPagina = false;
+                }
+
+                numeroQuestao++;
             }
 
-            documento.Save(caminho);
+            // Desenha o número da última página
+            gfx.DrawString($"Página {numeroPagina}", fonteRodape, XBrushes.Black, new XRect(0, pagina.Height - margemInferior, pagina.Width - 40, fonteRodape.Height), XStringFormats.BottomRight);
 
-            //PdfDocument document = new PdfDocument();
-            //document.Info.Title = "Teste: " + teste.Titulo;
-
-            //PdfPage page = document.AddPage();
-            //page.Size = PageSize.A4;
-            //XGraphics gfx = XGraphics.FromPdfPage(page);
-            //XFont titleFont = new XFont("Verdana", 16, XFontStyleEx.Bold);//, XFontStyle.Bold
-            //XFont normalFont = new XFont("Verdana", 12);//, XFontStyle.Regular
-
-            //double yPos = 20;
-            //gfx.DrawString(teste.Titulo, titleFont, XBrushes.Black, new XRect(20, yPos, page.Width - 40, 30), XStringFormats.TopCenter);
-            //yPos += 40;
-
-            //gfx.DrawString("Disciplina: " + teste.Disciplina, normalFont, XBrushes.Black, new XRect(20, yPos, page.Width - 40, 20), XStringFormats.TopLeft);
-            //yPos += 20;
-
-            //gfx.DrawString("Matéria: " + teste.Materia, normalFont, XBrushes.Black, new XRect(20, yPos, page.Width - 40, 20), XStringFormats.TopLeft);
-            //yPos += 20;
-
-            //for (int i = 0; i < teste.Questoes.Count; i++)
-            //{
-            //    Questao questao = teste.Questoes[i];
-            //    gfx.DrawString($"{i + 1}. {questao.Enunciado}", normalFont, XBrushes.Black, new XRect(20, yPos, page.Width - 40, 20), XStringFormats.TopLeft);
-            //    yPos += 20;
-
-            //    for (int j = 0; j < questao.Alternativas.Count; j++)
-            //    {
-            //        gfx.DrawString($"   {((char)('A' + j))}. {questao.Alternativas[j]}", normalFont, XBrushes.Black, new XRect(40, yPos, page.Width - 40, 20), XStringFormats.TopLeft);
-            //        yPos += 20;
-            //    }
-
-            //    yPos += 10;
-            //}
-
-            //document.Save(caminho);
+            documento.Save(caminho);           
         }
 
         public void GerarPdfGabarito(Teste teste, string caminho)
         {
+
             PdfDocument documento = new PdfDocument();
-            documento.Info.Title = teste.Titulo;
+            documento.Info.Title = "Gabarito - " + teste.Titulo;
 
-            PdfPage pagina = documento.AddPage();
-            pagina.Size = PageSize.A4;
-
-            XGraphics gfx = XGraphics.FromPdfPage(pagina);
             XFont fonteTitulo = new XFont("Verdana", 20, XFontStyleEx.Bold);
             XFont fonteCabecalho = new XFont("Verdana", 14, XFontStyleEx.Bold);
             XFont fonteQuestao = new XFont("Verdana", 12, XFontStyleEx.Regular);
+            XFont fonteRodape = new XFont("Verdana", 10, XFontStyleEx.Regular);
 
+            int numeroPagina = 1;
+            double margemInferior = 40;
+
+            // Variável para controlar o número de questões por página
+            int questoesPorPagina = 0;
+
+            PdfPage pagina = documento.AddPage();
+            pagina.Size = PageSize.A4;
+            XGraphics gfx = XGraphics.FromPdfPage(pagina);
+
+            // Define a posição inicial yPoint
             double yPoint = 40;
 
-            gfx.DrawString(teste.Titulo, fonteTitulo, XBrushes.Black, new XRect(0, yPoint, pagina.Width, pagina.Height), XStringFormats.TopCenter);
+            // Desenha o título
+            gfx.DrawString($"Gabarito - {teste.Titulo}", fonteTitulo, XBrushes.Black, new XRect(0, yPoint, pagina.Width, pagina.Height), XStringFormats.TopCenter);
             yPoint += 40;
 
+            // Desenha a disciplina e a matéria
             gfx.DrawString($"Disciplina: {teste.Disciplina.Nome}", fonteCabecalho, XBrushes.Black, new XRect(40, yPoint, pagina.Width, pagina.Height), XStringFormats.TopLeft);
             yPoint += 30;
-            gfx.DrawString($"Matéria: {teste.Materia.Nome}", fonteCabecalho, XBrushes.Black, new XRect(40, yPoint, pagina.Width, pagina.Height), XStringFormats.TopLeft);
-            yPoint += 30;
+            if(teste.Materia != null)
+            {
+                gfx.DrawString($"Matéria: {teste.Materia.Nome}", fonteCabecalho, XBrushes.Black, new XRect(40, yPoint, pagina.Width, pagina.Height), XStringFormats.TopLeft);
+                yPoint += 30;
+            }
+            else
+            {
+                gfx.DrawString($"Matéria: Prova de Recuperação", fonteCabecalho, XBrushes.Black, new XRect(40, yPoint, pagina.Width, pagina.Height), XStringFormats.TopLeft);
+                yPoint += 30;
+            }
+
+            int numeroQuestao = 1;
 
             foreach (var questao in teste.Questoes)
             {
-                if (yPoint > pagina.Height - 100)
+                // Verifica se é necessário adicionar uma nova página
+                if ((numeroQuestao > 5 && questoesPorPagina == 0) || questoesPorPagina == 6)
                 {
+                    // Desenha o número da página atual
+                    gfx.DrawString($"Página {numeroPagina}", fonteRodape, XBrushes.Black, new XRect(0, pagina.Height - margemInferior, pagina.Width - 40, fonteRodape.Height), XStringFormats.BottomRight);
+                    numeroPagina++;
+
+                    // Adiciona uma nova página e redefine yPoint
                     pagina = documento.AddPage();
                     pagina.Size = PageSize.A4;
                     gfx = XGraphics.FromPdfPage(pagina);
                     yPoint = 40;
+
+                    // Redefine o contador de questões por página
+                    questoesPorPagina = 0;
                 }
 
-                gfx.DrawString(questao.Enunciado, fonteQuestao, XBrushes.Black, new XRect(40, yPoint, pagina.Width - 80, pagina.Height), XStringFormats.TopLeft);
+                // Desenha a questão
+                gfx.DrawString($"{numeroQuestao} - {questao.Enunciado}", fonteQuestao, XBrushes.Black, new XRect(40, yPoint, pagina.Width - 80, pagina.Height), XStringFormats.TopLeft);
                 yPoint += 20;
 
+                // Desenha as alternativas, marcando a correta em verde
                 foreach (var alternativa in questao.Alternativas)
                 {
                     if (alternativa.AlternativaCorreta)
@@ -140,48 +197,101 @@ namespace WinFormsApp.ModuloTeste
                     yPoint += 20;
                 }
                 yPoint += 20;
+
+                numeroQuestao++;
+                questoesPorPagina++;
+
+                // Após a quinta questão na primeira página, mudar para 6 questões por página
+                if (numeroQuestao == 6)
+                {
+                    questoesPorPagina = 0;
+                }
             }
+
+            // Desenha o número da última página
+            gfx.DrawString($"Página {numeroPagina}", fonteRodape, XBrushes.Black, new XRect(0, pagina.Height - margemInferior, pagina.Width - 40, fonteRodape.Height), XStringFormats.BottomRight);
 
             documento.Save(caminho);
 
-            //PdfDocument document = new PdfDocument();
-            //document.Info.Title = teste.Titulo;
+            //PdfDocument documento = new PdfDocument();
+            //documento.Info.Title = "Gabarito - " + teste.Titulo;
 
-            //PdfPage page = document.AddPage();
-            //XGraphics gfx = XGraphics.FromPdfPage(page);
-            //XFont titleFont = new XFont("Verdana", 20);//, XFontStyle.Bold
-            //XFont headerFont = new XFont("Verdana", 14);//, XFontStyle.Bold
-            //XFont regularFont = new XFont("Verdana", 12);//, XFontStyle.Regular
-            //XFont correctAnswerFont = new XFont("Verdana", 12);//, XFontStyle.Bold
+            //XFont fonteTitulo = new XFont("Verdana", 20, XFontStyleEx.Bold);
+            //XFont fonteCabecalho = new XFont("Verdana", 14, XFontStyleEx.Bold);
+            //XFont fonteQuestao = new XFont("Verdana", 12, XFontStyleEx.Regular);
+            //XFont fonteRodape = new XFont("Verdana", 10, XFontStyleEx.Regular);
 
-            //int yPoint = 40;
+            //int numeroPagina = 1;
+            //double margemInferior = 40;
+            //int questoesPorPagina = 0;
 
-            //gfx.DrawString(teste.Titulo, titleFont, XBrushes.Black, new XRect(0, yPoint, page.Width, page.Height), XStringFormats.TopCenter);
+            //PdfPage pagina = documento.AddPage();
+            //pagina.Size = PageSize.A4;
+            //XGraphics gfx = XGraphics.FromPdfPage(pagina);
+
+            //// Define a posição inicial yPoint
+            //double yPoint = 40;
+
+            //// Desenha o título
+            //gfx.DrawString($"Gabarito - {teste.Titulo}", fonteTitulo, XBrushes.Black, new XRect(0, yPoint, pagina.Width, pagina.Height), XStringFormats.TopCenter);
             //yPoint += 40;
 
-            //gfx.DrawString($"Disciplina: {teste.Disciplina}", headerFont, XBrushes.Black, new XRect(40, yPoint, page.Width, page.Height), XStringFormats.TopLeft);
+            //// Desenha a disciplina e a matéria
+            //gfx.DrawString($"Disciplina: {teste.Disciplina.Nome}", fonteCabecalho, XBrushes.Black, new XRect(40, yPoint, pagina.Width, pagina.Height), XStringFormats.TopLeft);
+            //yPoint += 30;
+            //gfx.DrawString($"Matéria: {teste.Materia.Nome}", fonteCabecalho, XBrushes.Black, new XRect(40, yPoint, pagina.Width, pagina.Height), XStringFormats.TopLeft);
             //yPoint += 30;
 
-            //gfx.DrawString($"Matéria: {teste.Materia.Nome}", headerFont, XBrushes.Black, new XRect(40, yPoint, page.Width, page.Height), XStringFormats.TopLeft);
-            //yPoint += 30;
+            //int numeroQuestao = 1;
 
-            //for (int i = 0; i < teste.Questoes.Count; i++)
+            //foreach (var questao in teste.Questoes)
             //{
-            //    var questao = teste.Questoes[i];
-            //    gfx.DrawString($"{i + 1}. {questao.Enunciado}", regularFont, XBrushes.Black, new XRect(40, yPoint, page.Width, page.Height), XStringFormats.TopLeft);
-            //    yPoint += 25;
-
-            //    for (int j = 0; j < questao.Alternativas.Count; j++)
+            //    // Verifica se é necessário adicionar uma nova página
+            //    if ((numeroQuestao > 5 && questoesPorPagina == 0) || questoesPorPagina == 6)
             //    {
-            //        bool isCorrect = questao.Alternativas[j].AlternativaCorreta;
-            //        gfx.DrawString($"{(char)('A' + j)}. {questao.Alternativas[j].TextoAlternativa}", isCorrect ? correctAnswerFont : regularFont, isCorrect ? XBrushes.Green : XBrushes.Black, new XRect(60, yPoint, page.Width, page.Height), XStringFormats.TopLeft);
-            //        yPoint += 20;
+            //        // Desenha o número da página atual
+            //        gfx.DrawString($"Página {numeroPagina}", fonteRodape, XBrushes.Black, new XRect(0, pagina.Height - margemInferior, pagina.Width - 40, fonteRodape.Height), XStringFormats.BottomRight);
+            //        numeroPagina++;
+
+            //        // Adiciona uma nova página e redefine yPoint
+            //        pagina = documento.AddPage();
+            //        pagina.Size = PageSize.A4;
+            //        gfx = XGraphics.FromPdfPage(pagina);
+            //        yPoint = 40;
+
+            //        // Redefine o contador de questões por página
+            //        questoesPorPagina = 0;
             //    }
 
-            //    yPoint += 10;
+            //    // Desenha a questão
+            //    gfx.DrawString($"{numeroQuestao} - {questao.Enunciado}", fonteQuestao, XBrushes.Black, new XRect(40, yPoint, pagina.Width - 80, pagina.Height), XStringFormats.TopLeft);
+            //    yPoint += 20;
+
+            //    // Desenha as alternativas, marcando a correta em verde
+            //    foreach (var alternativa in questao.Alternativas)
+            //    {
+            //        if (alternativa.AlternativaCorreta)
+            //        {
+            //            gfx.DrawString($"{alternativa.Letra}) {alternativa.TextoAlternativa}", fonteQuestao, XBrushes.Green, new XRect(60, yPoint, pagina.Width - 80, pagina.Height), XStringFormats.TopLeft);
+            //        }
+            //        else
+            //        {
+            //            gfx.DrawString($"{alternativa.Letra}) {alternativa.TextoAlternativa}", fonteQuestao, XBrushes.Black, new XRect(60, yPoint, pagina.Width - 80, pagina.Height), XStringFormats.TopLeft);
+            //        }
+            //        yPoint += 20;
+            //    }
+            //    yPoint += 20;
+
+            //    numeroQuestao++;
+            //    questoesPorPagina++;
             //}
 
-            //document.Save(caminho);
+            //// Desenha o número da última página
+            //gfx.DrawString($"Página {numeroPagina}", fonteRodape, XBrushes.Black, new XRect(0, pagina.Height - margemInferior, pagina.Width - 40, fonteRodape.Height), XStringFormats.BottomRight);
+
+            //documento.Save(caminho);        
+
         }
+        
     }
 }
